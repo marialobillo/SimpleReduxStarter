810b9f1632097002546e28ff811d6f116c4a7454
Maria Lobillo <maria.lobillo.santos@gmail.com>
2018-04-11 20:08:59 +0200

    structure and youtube api key

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const API_KEY = 'AIzaSyDF9zOIKzOW6sHeSx7FvIK0nMWg7ZFB0OI';
+
 // Create a new component
 const App = () => {
 	return <div>Hi!</div>;
