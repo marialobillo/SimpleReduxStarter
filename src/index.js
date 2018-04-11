0d1b8197c505d5a882b3e9ad934f130ef247134f
Maria Lobillo <maria.lobillo.santos@gmail.com>
2018-04-11 18:54:02 +0200

    hi again

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Create a new component
+const App = () => {
+	return <div>Hi!</div>;
+}
+
+
+ReactDOM.render(<App/>, document.getElementById('root'));
