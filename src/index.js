a382f3b137d983d187e760811d50b1fda404e5cb
Maria Lobillo <maria.lobillo.santos@gmail.com>
2018-04-13 16:56:54 +0200

    props

--- src/index.js
+++ src/index.js
@@ -2,18 +2,26 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyAR__yp368axr1-DO2SycYrn2HbXVPjcgY';
 
-YTSearch({key: API_KEY, term:'suftboards'}, function(data){
-	console.log(data);
-});
 
 
 class App extends Component{
+	constructor(props){
+		super(props);
+
+		this.state = { videos: [] };
+
+		YTSearch({key: API_KEY, term:'suftboards'}, (videos) => {
+			this.setState( { videos: videos });
+		});
+	}
 	render(){
 		return(
 			<div>
-				<SearchBar/>
+				<SearchBar />
+				<VideoList videos={this.state.videos}/>
 			</div>
 		);
 	}
